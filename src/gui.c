4d7688b023d785cdb6f84d52c44c0af364656550
0xF3d04a <0xF3d04a@users.noreply.github.com>
2015-12-14 13:52:32 -0500

    Added GtkWidgetGroup for my window

--- src/gui.c
+++ src/gui.c
@@ -1,11 +1,13 @@
 #include <gtk/gtk.h>
 
+typedef GtkWidget *GtkWidgetGroup[];
+
 GtkWidget *init_gui() {
-  GtkWidget *window;
+  GtkWidgetGroup window;
   gtk_init(&argc, &argv);
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  g_signal_connect(window, "delete-event", G_CALLBACK (DeleteEvent), NULL);
-  g_signal_connect(window, "destroy", G_CALLBACK (WindowClose), NULL);
-  gtk_widget_show(window);
+  window[0] = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  g_signal_connect(window[0], "delete-event", G_CALLBACK (DeleteEvent), NULL);
+  g_signal_connect(window[0], "destroy", G_CALLBACK (WindowClose), NULL);
+  gtk_widget_show(window[0]);
   return window;
 }
